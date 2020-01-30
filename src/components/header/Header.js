cd02773fb6269704947513f8a4c06c89c1cff234
Shaurya Singhal(jugshaurya) <shauryasinghal84@gmail.com>
2020-01-30 12:05:04 +0530

    :boom: added active class to navbar links, offset the html anchors to adjust fixed header, a small a:hover effect on email address, added Error boundary component as well

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -5,8 +5,15 @@ import "./Header.scss";
 class Header extends React.Component {
   state = {
     isCollapsedNavOpen: false,
-    height: 0
+    height: 0,
+    activeLink: "about"
   };
+
+  componentDidMount() {
+    if (2 === 2) {
+      throw new Error("sad");
+    }
+  }
 
   showNavbar = val => {
     const obj = {
@@ -16,18 +23,64 @@ class Header extends React.Component {
     this.setState(obj);
   };
 
+  handleActiveLink = item => {
+    this.setState({ activeLink: item });
+  };
+
   render() {
     return (
       <header>
         <div className="name">Shaurya Singhal</div>
         <div className="navbar">
-          <a href="#about">About Me</a>
-          <a href="#workstats">Work Stats</a>
-          <a href="#projects">Projects</a>
-          <a href="#experience">Experience</a>
-          <a href="#education">Education</a>
-          <a href="#skills">Skills</a>
-          {/* <a to="#blogs">Blogs</a> */}
+          <a
+            href="#about"
+            className={this.state.activeLink === "about" ? "active" : ""}
+            onClick={() => this.handleActiveLink("about")}
+          >
+            About Me
+          </a>
+          <a
+            href="#workstats"
+            className={this.state.activeLink === "workstats" ? "active" : ""}
+            onClick={() => this.handleActiveLink("workstats")}
+          >
+            Work Stats
+          </a>
+          <a
+            href="#projects"
+            className={this.state.activeLink === "projects" ? "active" : ""}
+            onClick={() => this.handleActiveLink("projects")}
+          >
+            Projects
+          </a>
+          <a
+            href="#experience"
+            className={this.state.activeLink === "experience" ? "active" : ""}
+            onClick={() => this.handleActiveLink("experience")}
+          >
+            Experience
+          </a>
+          <a
+            href="#education"
+            className={this.state.activeLink === "education" ? "active" : ""}
+            onClick={() => this.handleActiveLink("education")}
+          >
+            Education
+          </a>
+          <a
+            href="#skills"
+            className={this.state.activeLink === "skills" ? "active" : ""}
+            onClick={() => this.handleActiveLink("skills")}
+          >
+            Skills
+          </a>
+          {/* <a
+            to="#blogs"
+            className={this.state.activeLink === "blogs" ? "active" : ""}
+            onClick={() => this.handleActiveLink("blogs")}
+          >
+            Blogs
+          </a> */}
         </div>
         <div className="navbar-collapse" onClick={() => this.showNavbar(true)}>
           &#9776;
